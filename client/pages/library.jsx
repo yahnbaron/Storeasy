6b8d49da059a67ba5a513692da83e28e62730c22
Baron Yahn <yahnbaron@me.com>
2023-01-22 05:02:19 +0000

    first quick layout of library.jsx

--- client/pages/library.jsx
+++ client/pages/library.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import NavBar from '../components/nav-bar';
 
-export default function Library(props) {
+function Library() {
+  const [stories, setStories] = useState([]);
+  useEffect(() => {
+    fetch('/api/getstories')
+      .then(res => res.json())
+      .then(data => {
+        setStories(data.stories);
+      });
+  }, []);
   return (
     <div>
       <NavBar />
       <div className='container'>
-        <h2>Welcome to The Library</h2>
+        <h2>Library</h2>
+        {
+          stories.map(story => (
+            <button key={story.storyId}>{story.title}</button>
+          ))
+        }
       </div>
     </div>
   );
 }
+export default Library;
